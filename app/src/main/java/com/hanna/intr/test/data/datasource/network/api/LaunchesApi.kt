3e6eeb136ceb641fe2b715bb0cd3c486bf85046c
HannaShulman <hsc@cookieinformation.com>
2024-06-28 01:38:42 +0100

    add getLaunchById- api

--- app/src/main/java/com/hanna/intr/test/data/datasource/network/api/LaunchesApi.kt
+++ app/src/main/java/com/hanna/intr/test/data/datasource/network/api/LaunchesApi.kt
@@ -2,8 +2,12 @@ package com.hanna.intr.test.data.datasource.network.api
 
 import com.hanna.intr.test.data.datasource.network.responses.LaunchDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface LaunchApi {
     @GET("v4/launches")
     suspend fun getLaunches(): List<LaunchDto>
+
+    @GET("v4/launches/{Id}")
+    suspend fun getLaunchById(@Path("Id") id: String): LaunchDto
 }
